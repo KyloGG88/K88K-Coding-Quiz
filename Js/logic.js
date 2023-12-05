fff7e45ba8383b34c2c97d115c09f77203c62236
Kyle GG <goliath.kylec@gmail.com>
2023-12-05 17:06:02 +0000

    Dom elements

---
+++ Js/logic.js
@@ -0,0 +1,11 @@
+// DOM elements
+
+let questionsEl = document.querySelector('#questions');
+let timerEl = document.querySelector('#timer');
+let choicesEl = document.querySelector('#options');
+let submitBtnEl = document.querySelector('#submit');
+let startBtnEl = document.querySelector('#start');
+let nameEl = document.querySelector('#name');
+let feedbackEl = document.querySelector('#feedback');
+let gobackEl = document.querySelector('#goback');
+
