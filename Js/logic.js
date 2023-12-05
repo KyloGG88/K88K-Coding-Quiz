294d4dcd11661fe9d08eb4acf9edc0ff91b45df5
Kyle GG <goliath.kylec@gmail.com>
2023-12-05 21:29:04 +0000

    right or wrong answers

--- Js/logic.js
+++ Js/logic.js
@@ -14,7 +14,7 @@ let currentQuestionIndex = 0;
 let time = questions.length * 15;
 let timerId;
 
-// Starting the  quiz and hiding the initial page
+// Starting the quiz and hiding the initial page
 
 function quizStart() { 
   timerID = setInterval(
@@ -30,13 +30,56 @@ function quizStart() {
   getQuestions();
 }
 
+// Retrieve questions from the Object
+
 function getQuestions() { 
   let currentQuestion = questions[currentQuestionIndex];
   let promptEl = document.getElementById('#question-title');
 
   promptEl.textContent = currentQuestion.prompt;
   choicesEl.innerHTML = "";
-  
+  currentQuestionIndex.options.forEach(
+    function (choice, i) {
+      let choiceBtn = document.createElementById("button");
+
+      choiceBtn.setAttribute("value", choice);
+      choiceBtn.textContent = i + 1 + ". " + choice;
+      choiceBtn.onclick = questionClick;
+      choicesEl.appendChild(choiceBtn);
+    }
+  );  
 }
+
+//Right or Wrong Answers function
+
+function questionClick() { 
+  if (this.value !== questions[currentQuestionIndex].answer) { 
+    time -= 5;
+    if (time < 0) { 
+        time = 0; 
+      } 
+      timerEl.textContent = time; 
+      feedbackEl.textContent = `Wrong! The correct answer was  
+      ${questions[currentQuestionIndex].answer}.`; 
+      feedbackEl.style.color = "red"; 
+  } else { 
+      feedbackEl.textContent = "Correct!"; 
+      feedbackEl.style.color = "green"; 
+  } 
+  feedbackEl.setAttribute( 
+      "class", 
+      "feedback"
+  ); 
+  setTimeout(function () { 
+      feedbackEl.setAttribute( 
+          "class", "feedback hide"); 
+  }, 2000); 
+  currentQuestionIndex++; 
+  if (currentQuestionIndex === questions.length) { 
+      quizEnd(); 
+  } else { 
+      getQuestion(); 
+  } 
+} 
 
 startBtn.onclick = quizStart;
