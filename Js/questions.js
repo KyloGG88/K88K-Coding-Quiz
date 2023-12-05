16a3719cfaedee9ad1e2b7cd43d948b476d1e762
Kyle GG <goliath.kylec@gmail.com>
2023-12-05 16:50:45 +0000

    questions setup

---
+++ Js/questions.js
@@ -0,0 +1,42 @@
+let questions = [
+  { 
+    prompt: `Declaring varaiables using "const" ...Which describes "const" best?`, 
+    options: [ 
+        "Variables can be reassigned", 
+        "Allows variables to be constant", 
+        "It declares an unchanging variable", 
+        "It avoids errors in console", 
+    ], 
+    answer: "It declares an unchanging variable", 
+  },
+  { 
+    prompt: `How are comments added to the Javascript file?`, 
+    options: [ 
+        "//", 
+        "/* */", 
+        "/__/", 
+        "()", 
+    ], 
+    answer: "//", 
+  },
+  { 
+    prompt: `How can we add comments to our Javascript?`, 
+    options: [ 
+        "for (i = 0; i < 10; i++)", 
+        "for i = 1 + 9", 
+        "for (i, || = 10; i++)", 
+        "for (i = 10; i <= 10)", 
+    ], 
+    answer: "for (i = 0; i < 10; i++)", 
+  },
+  { 
+    prompt: `Which of tthe following is a logical operator in Javascript?`, 
+    options: [ 
+        "//", 
+        "%", 
+        "||", 
+        "()", 
+    ], 
+    answer: "||", 
+  },
+];
