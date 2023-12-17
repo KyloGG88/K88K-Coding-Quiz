dc4a0736f05588342608649e7f36ac6c6b537012
Kyle GG <goliath.kylec@gmail.com>
2023-12-17 15:31:38 +0000

    assets folder correction

--- Assets/Js/logic.js
+++ Assets/Js/logic.js
@@ -11,8 +11,8 @@ var feedbackEl = document.querySelector("#feedback");
 var currentQuestionIndex = 0;
 var time = questions.length * 10;
 var timerId;
-var right = "./sfx/correct.mp3";
-var wrong = "./sfx/incorrect.mp3";
+var right = "Assets/sfx/correct.mp3";
+var wrong = "Assets/sfx/incorrect.mp3";
 
 
 // Start function
